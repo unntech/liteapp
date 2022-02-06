19de778d62ca3c1f51c94946906066dd90954a76
lzs9605 <lzs9605@gmail.com>
2022-02-06 23:07:03 +0800

    add lite icon

--- app/LiteApp.php
+++ app/LiteApp.php
@@ -10,7 +10,7 @@ class LiteApp
     /**
      * 配置参数
      */
-    const VERSION = '1.0.3';
+    const VERSION = '1.0.4';
     public $config, $db, $redis;
     public $DT_TIME;
     public $appName;
--- tests/t1.php
+++ tests/t1.php
@@ -6,7 +6,7 @@ require __DIR__.'/../autoload.php';
 
 $config = $Lite->config->get('app');
 
-echo ip2long('10.100.112.3');
-echo long2ip(174354435);
 
 \LitePhp\Template::message('这是一个提示示例', '错误提示');
+
+

c9c22e7c916554b46c4a06f92db18f66256492b3
lzs9605 <lzs9605@gmail.com>
2023-06-15 03:08:36 +0800

    update

---
+++ admin/t1.php
@@ -0,0 +1,19 @@
+<?php
+
+require '../autoload.php';
+$activeMenu = 0;
+require 'auth.inc.php';
+
+//$menuToken = $liCrypt->getToken($auth->menu);
+//$nodeToken = $liCrypt->getToken($auth->node);
+//var_dump($menuToken, $nodeToken);
+
+/*
+$Lite->set_redis();
+var_dump($Lite->redis->set('a','adb', 3600));
+$a = $Lite->redis->get('a');
+LitePhp\Redis::set('b', 'abc', 3600);
+*/
+
+session_start();
+var_dump(session_id());

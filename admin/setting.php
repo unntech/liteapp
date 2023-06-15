8b8cca4804234833890fe4959451a6488efe308e
lzs9605 <lzs9605@gmail.com>
2023-06-15 12:11:25 +0800

    update col max-md

--- admin/setting.php
+++ admin/setting.php
@@ -4,6 +4,9 @@ require '../autoload.php';
 $activeMenu = 4;
 require 'auth.inc.php';
 
+if(isset($_POST['saveBtn'])){
+    echo 'post';
+}
 
 $CSS = ['admin'];
 include LitePhp\Template::load('setting', 'admin');

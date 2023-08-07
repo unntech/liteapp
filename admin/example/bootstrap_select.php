97008393bde4d5c50abb942fbc3bf237d9e2f7dc
lzs9605 <lzs9605@gmail.com>
2023-08-07 10:24:37 +0800

    bootstrap-select

---
+++ admin/example/bootstrap_select.php
@@ -0,0 +1,10 @@
+<?php
+
+require '../../autoload.php';
+$activeMenu = 0;
+require '../auth.inc.php';
+
+$list1 = ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'];
+
+$CSS = ['admin'];
+include LitePhp\Template::load('bootstrap_select', 'admin/example');
